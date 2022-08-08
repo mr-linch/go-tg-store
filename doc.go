fa52b44782aab33cc0af733082180ae1d7fb566d
Sasha Savchuk <sasha@bots.house>
2022-08-08 16:34:35 +0300

    chore: make main module

---
+++ doc.go
@@ -0,0 +1,3 @@
+// This module contains different implementations of the session store for [github.com/mr-linch/go-tg/tgb/session.Store]
+// Each implementation has its module and should be go get before use.
+package store
